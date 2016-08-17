6bcd0c67b0a74f1f2c7bdefdff77a0547779f984
Zac Laudick <zlaudick@nmsu.edu>
2016-08-17 08:18:51 -0600

    added navbar

--- index.php
+++ index.php
@@ -29,5 +29,47 @@
 		<title>Bootstrap Challenge</title>
 	</head>
 	<body>
+		<header>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+
+						<!-- begin navbar -->
+						<nav class="navbar navbar-inverse">
+							<!-- Brand and toggle get grouped for better mobile display -->
+							<div class="navbar-header">
+								<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+									<span class="sr-only">Toggle navigation</span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+								<a class="navbar-brand" href="#">My Bootstrap Page</a>
+							</div>
+
+							<!-- Collect the nav links, forms, and other content for toggling -->
+							<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+								<ul class="nav navbar-nav navbar-right">
+									<li><a href="#">Link</a></li>
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Moar Links <span class="caret"></span></a>
+										<ul class="dropdown-menu">
+											<li><a href="#">Action</a></li>
+											<li><a href="#">Another action</a></li>
+											<li><a href="#">Something else here</a></li>
+											<li role="separator" class="divider"></li>
+											<li><a href="#">Separated link</a></li>
+										</ul>
+									</li>
+								</ul>
+							</div><!-- /.navbar-collapse -->
+
+						</nav>
+					</div>
+				</div>
+			</div>
+		</header>
+		<main></main>
+		<footer></footer>
 	</body>
 </html>
